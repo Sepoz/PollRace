6b8215273b4dac6f40d7ea5be75d3e0009ec2346
Simone Boccolari <simone.boccolari@hotmail.it>
2020-07-05 17:55:37 +0200

    caching

--- routes/pollRoute.js
+++ routes/pollRoute.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const Data = require('./module/wikiScrape.js');
+const Data = require('../module/wikiScrape.js');
+
+let cacheData;
+let cacheTime;
+let cachePNum;
 
 router.get('/polls/:pNum', async (req, res) => {
-  const num = Number(req.params.pNum);
-  let getPolls = await Data.getWikiPage(num);
-  res.json(getPolls);
+  if(cacheTime && cacheTime > Date.now() - 1000 * 600) {
+    if(cachePNum && cachePNum === req.params.pNum) {
+      return res.json(cacheData);
+    }
+  }
+  try {
+    const num = Number(req.params.pNum);
+    let getPolls = await Data.getWikiPage(num);
+    cacheData = getPolls;
+    cacheTime = Date.now();
+    cachePNum = req.params.pNum;
+    return res.json(getPolls);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
